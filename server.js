f57485034f5342790f64cbf6dcbc860ea8dc4dfd
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-10-09 15:51:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const ejs = require('ejs');
 
 
 app.use(cors({
-  origin: 'https://vcardclient.vercel.app',
+  origin: 'https://vcardclient-gzxrsr1hn-raj-24642cc0.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
