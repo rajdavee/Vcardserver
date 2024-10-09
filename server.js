29250bd787bea0e34ef232f7ce5d228ce942f15c
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-10-09 16:55:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const ejs = require('ejs');
 
 
 app.use(cors({
-origin: ['https://vcardclient-qd0anahzk-raj-24642cc0.vercel.app'],
+origin: ['https://effervescent-chimera-8ae9ee.netlify.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
