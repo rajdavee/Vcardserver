e93eed45803340f7816145c3b3f4c544b4ebcb68
rajdavee <rajdave710@gmail.com>
2024-10-17 11:45:56 +0530

    removed geo ip module

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,6 @@
   const path = require('path');
   const crypto = require('crypto');
   const VCardScan = require('../models/VCardScan');
-  const geoip = require('geoip-lite');
   const axios = require('axios');
   const mongoose = require('mongoose');
 
